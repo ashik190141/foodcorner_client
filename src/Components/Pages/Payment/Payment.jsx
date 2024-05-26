df96a42b2edf0f2fa326e83ef0121f8e91c0c34d
ashik190141 <ashanmahmudashik@gmail.com>
2024-05-26 23:46:36 +0600

    payment history

--- src/Components/Pages/Payment/Payment.jsx
+++ src/Components/Pages/Payment/Payment.jsx
@@ -1,23 +1,64 @@
 import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
-import React from 'react';
+import React, { useContext } from 'react';
 import { useLocation } from 'react-router-dom';
 import useTitle from '../../../Hooks/useTitle';
 import CheckoutForm from './CheckoutForm';
 
+import Modal from "react-modal";
+import { AuthContext } from '../../Providers/AuthProviders/AuthProviders';
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
 
 const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GATEWAY_PK);
+import PaymentHistory from './PaymentHistory';
 
 const Payment = () => {
     useTitle("Payment");
+    const {user} = useContext(AuthContext);
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+
+    function openModal() {
+      setIsOpen(true);
+    }
+
+    function closeModal() {
+      setIsOpen(false);
+    }
 
     const location = useLocation();
     const price = location.state;
     return (
-      <div className="p-11 mt-20">
+      <div className="p-11 mt-20 max-w-7xl mx-auto">
+        <div className="flex justify-end">
+          <button onClick={openModal}>Payment History</button>
+        </div>
         <Elements stripe={stripePromise}>
           <CheckoutForm price={price}></CheckoutForm>
         </Elements>
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+          <div className='flex justify-end'>
+            <button className="text-2xl" onClick={closeModal}>
+              X
+            </button>
+          </div>
+          <PaymentHistory email={user?.email}></PaymentHistory>
+        </Modal>
       </div>
     );
 };
---
+++ src/Components/Pages/Payment/PaymentHistory.jsx
@@ -0,0 +1,68 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { useDateFormatter } from "../../../Hooks/useDateFormatter";
+import UploadingSpinner from "../../Shared/LoadSpinner/UploadingSpinner";
+
+const PaymentHistory = ({ email }) => {
+  console.log(email);
+  const [paymentHistory, setPaymentHistory] = useState([]);
+  const [show, setShow] = useState(true);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/payment-history/${email}`, {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    })
+      .then((res) => res.json())
+        .then((data) => {
+            setShow(false)
+            setPaymentHistory(data)
+        });
+  }, [email]);
+  return (
+    <div className="max-w-7xl mx-auto">
+      {!show ? (
+        <div>
+          {paymentHistory.length != 0 ? (
+            <div className="flex flex-col items-center justify-center">
+              <h3 className="text-2xl font-semibold my-4 text-center mt-10 mb-10">
+                Payment History
+              </h3>
+              <div className="overflow-x-auto">
+                <table className="table table-zebra">
+                  {/* head */}
+                  <thead>
+                    <tr>
+                      <th className="text-xl">#</th>
+                      <th className="text-xl text-center">Date</th>
+                      <th className="text-xl text-center">Transaction ID</th>
+                      <th className="text-xl text-center">Price</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {paymentHistory.map((user, index) => (
+                      <tr key={user._id}>
+                        <th className="text-xl">{index + 1}</th>
+                        <td className="text-xl">
+                          {useDateFormatter(user?.date)}
+                        </td>
+                        <td className="text-xl">{user.transactionId}</td>
+                        <td className="text-xl text-center">$ {user.price}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          ) : <div className="py-5 text-xl">You Have No Payment History</div>}
+        </div>
+      ) : (
+        <UploadingSpinner></UploadingSpinner>
+      )}
+    </div>
+  );
+};
+
+export default PaymentHistory;
